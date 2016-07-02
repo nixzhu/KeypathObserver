d5f2a8dda2a2c8c6f002a4429cca371aa2a0bd06
nixzhu <zhuhongxu@gmail.com>
2016-07-02 15:38:23 +0800

    swift package

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  KeypathObserver
+//
+//  Created by NIX on 16/7/2.
+//  Copyright © 2016年 nixWork. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "KeypathObserver"
+)
+
+
